f8f74e00f6665f28d8cf1f2f3aa18fb6bb580987
François Moine <francois.moine@epitech.eu>
2021-03-08 16:42:55 +0100

    [*] bind base, app and routes to a Root component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,41 @@
 // Vendors
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+// Connections
+import useLang from 'connections/useLang';
+
+// Base
+import Base from './base';
+
+// Routes
+import { getRoutes } from './routes';
+
+// Components
+import App from 'components/app';
 
 function Root () {
 
+	const routes = getRoutes();
+	const { lang, setLang } = useLang();
+
 	return (
-		<React.Fragment>
-			Root 2 3 4 5 6
-		</React.Fragment>
+		<Base
+			lang_code={lang.code}
+		>
+			<App
+				routes={routes}
+				lang_code={lang.code}
+				onSwitchLang={setLang}
+			>
+				<Switch>
+					{routes.map(({ exact, Component, path, id }) => (
+						<Route key={id} path={path} exact={exact} component={Component} />
+					))}
+					<Route render={() => <Redirect to="/home" />} />
+				</Switch>
+			</App>
+		</Base>
 	);
 
 }
