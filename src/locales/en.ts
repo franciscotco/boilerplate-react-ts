3fb5884be70e1d4438f11f38b1c60bbec8d8fd42
François Moine <francois.moine@epitech.eu>
2021-03-08 16:32:35 +0100

    [*] add translations in english and french

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -1,3 +1,8 @@
 export default {
-
+	// COMPONENTS
+	'COMPONENTS.SECTION.BACK_URL.TITLE': 'Back',
+	// ROUTES
+	'ROUTES.HOME.NAME': 'Home',
+	'ROUTES.HOME.TITLE': 'Home',
+	'ROUTES.HOME.DESC': 'Description'
 };
---
+++ src/locales/index.ts
@@ -0,0 +1,8 @@
+// Locales
+import en from 'locales/en';
+import fr from 'locales/fr';
+
+export default {
+	en: en,
+	fr: fr
+};
