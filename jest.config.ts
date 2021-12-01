77c28eb0fb6745abd55facac613e224b0cc7c813
François MOINE <fmoine@gatling.io>
2021-12-01 02:55:10 +0100

    chore: refactor jest config in typescipt

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  rootDir: "src",
+  testMatch: ["**/__tests__/**/*.+(ts|tsx|js)", "**/?(*.)+(spec|test).+(ts|tsx|js)"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  },
+  moduleDirectories: ["node_modules", "src"],
+  moduleNameMapper: {
+    "@src(.*)$": "<rootDir>/$1"
+  }
+};
